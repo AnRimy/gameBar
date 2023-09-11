864eed98d15b9a7536e5fa9bd9086a20fdc94bd5
AnRimy <ardk03@bk.ru>
2023-09-11 22:21:48 +0300

    1

---
+++ controlMusic.py
@@ -0,0 +1,9 @@
+import sys
+from PyQt5.Qt import *
+
+class ControlMusic(QMainWindow):                             
+    def __init__(self, parent=None):
+        super().__init__(parent)                         
+        self.setWindowTitle("Window_2")
+        self.resize(200, 215)
+c
